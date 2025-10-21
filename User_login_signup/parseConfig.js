1b48483505f4891e898f7dc22f976f8d4c3bffdf
VictoriaFerrante <vferrante2022@fau.edu>
2025-10-21 17:43:21 -0400

    Got rid of an extra file.

--- User_login_signup/parseConfig.js
+++
@@ -1,4 +0,0 @@
-// parseConfig.js
-// Parse.initialize("fefJHvdGDQOAtrHXUOVnX62hq3s2KB8gUViNUWWP", "klHYFmiUyu9MhG0kVa4U5zjuyVyMD0oWpo33gHfb");
-// Parse.serverURL = "https://parseapi.back4app.com/";
-
